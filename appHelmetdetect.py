9023c387e1e3d06eecd7755dc0690ff024ac70d7
parnchaa <parnchaa@gmail.com>
2025-09-27 00:23:51 +0700

    Update appHelmetdetect.py

--- appHelmetdetect.py
+++ appHelmetdetect.py
@@ -12,11 +12,10 @@ model = YOLO("best.pt")
 
  # Upload image
 uploaded_image = st.file_uploader("Upload an image (jpg, png)", type=["jpg", "jpeg", "png"])
-
 if uploaded_image is not None:
 
  # Show original image
-st.image(uploaded_image, caption="Uploaded Image", use_container_width=True)
+ st.image(uploaded_image, caption="Uploaded Image", use_container_width=True)
 
  # Read image and convert to numpy array
 image = Image.open(uploaded_image)
